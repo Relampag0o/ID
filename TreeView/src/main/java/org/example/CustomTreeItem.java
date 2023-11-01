11f775d5b6d6daf829758d9729990fb42e066135
Relampago <josemariar.l17@gmail.com>
2023-11-01 15:26:46 +0100

    .

--- TreeView/src/main/java/org/example/CustomTreeItem.java
+++ TreeView/src/main/java/org/example/CustomTreeItem.java
@@ -1,9 +1,36 @@
 package org.example;
 
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TreeItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
 
 public class CustomTreeItem extends TreeItem<String> {
 
+    public File file;
+
+    public File getFile() {
+        return file;
+    }
+    
+
+    public CustomTreeItem(File f) {
+        super(f.getName());
+        this.file = f;
+        if (f.isDirectory()) {
+            ImageView folderIcon = new ImageView(new Image(getClass().getResourceAsStream("/folder.png")));
+            folderIcon.setFitWidth(20);
+            folderIcon.setFitHeight(20);
+            this.setGraphic(folderIcon);
+        } else if (f.isFile()) {
+            ImageView fileIcon = new ImageView(new Image(getClass().getResourceAsStream("/file.png")));
+            fileIcon.setFitWidth(20);
+            fileIcon.setFitHeight(20);
+            this.setGraphic(fileIcon);
+        }
+    }
 
 
 }
--- TreeView/src/main/java/org/example/PrimaryController.java
+++ TreeView/src/main/java/org/example/PrimaryController.java
@@ -1,9 +1,9 @@
 package org.example;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 
@@ -13,23 +13,46 @@ public class PrimaryController {
     public TreeView<String> treeV;
 
     @FXML
+    public TextArea textArea;
+
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
 
     public void initialize() {
         treeV.setRoot(create(new File(System.getProperty("user.dir"))));
+        treeV.getSelectionModel().selectedItemProperty().addListener(((observable, oldValue, newValue) -> {
+            textArea.setText("");
+            if (newValue != null) {
+                try {
+                    CustomTreeItem cti = (CustomTreeItem) newValue;
+                    BufferedReader bfr = new BufferedReader(new FileReader(cti.getFile()));
+                    String line = "";
+                    while ((line = bfr.readLine()) != null) {
+                        textArea.appendText(line + '\n');
+                    }
+                    bfr.close();
+                } catch (Exception e) {
+
+                }
+
+            }
+
+        }));
+
     }
 
 
     public TreeItem<String> create(File f) {
         File[] dirs = f.listFiles();
-        TreeItem<String> treeItem = new TreeItem<String>(f.getName());
+        CustomTreeItem treeItem = new CustomTreeItem(f);
         for (File file : dirs) {
             if (file.isDirectory())
                 treeItem.getChildren().add(create(file));
+
             else {
-                TreeItem<String> treeIt = new TreeItem<String>(file.getName());
+                CustomTreeItem treeIt = new CustomTreeItem(file);
                 treeItem.getChildren().add(treeIt);
             }
         }
