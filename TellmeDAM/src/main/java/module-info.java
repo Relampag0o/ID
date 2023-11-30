89414b3bc07b96157f85753faafda7350d9842bb
Relampag0o <josemariar.l17@gmail.com>
2023-11-30 16:55:53 +0100

    .
    .

--- TellmeDAM/src/main/java/module-info.java
+++ TellmeDAM/src/main/java/module-info.java
@@ -2,6 +2,15 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens org.example to javafx.fxml;
+    requires com.fasterxml.jackson.databind;
+    requires okhttp3;
+    requires com.google.gson;
+    requires okhttp3.logging;
+    requires java.net.http;
+    requires MaterialFX;
+
+    opens org.example to com.google.gson, javafx.fxml;
     exports org.example;
+
+
 }
