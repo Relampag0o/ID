92aba5d0807d32e1f98b7b5d012e7eca225ff184
Relampag0o <josemariar.l17@gmail.com>
2023-11-30 18:35:36 +0100

    .
    .

--- TellmeDAM/src/main/java/org/example/PrimaryController.java
+++ TellmeDAM/src/main/java/org/example/PrimaryController.java
@@ -1,12 +1,47 @@
 package org.example;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import org.example.api.APICallback;
+import org.example.api.UserAPIClient;
 
 public class PrimaryController {
+
+    public TextField passwordField;
+    public TextField emailField;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+
     }
+
+    public void login() {
+        UserAPIClient user = new UserAPIClient();
+        try {
+            try {
+                user.login(emailField.getText(), passwordField.getText(), new APICallback() {
+                    @Override
+                    public void onSuccess(Object response) throws IOException {
+                        System.out.println("Success logging.");
+                    }
+
+                    @Override
+                    public void onError(Object error) {
+                        System.out.println("Error logging");
+                    }
+                });
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+
 }
--- TellmeDAM/src/main/java/org/example/SecondaryController.java
+++ TellmeDAM/src/main/java/org/example/SecondaryController.java
@@ -1,12 +1,49 @@
 package org.example;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import org.example.api.APICallback;
+import org.example.api.RootAPIClient;
+import org.example.api.UserAPIClient;
 
 public class SecondaryController {
+
+    public TextField emailField;
+    public TextField passwordField;
+    public TextField nameField;
+    public TextField addressField;
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
+
+    public void registerUser() {
+        UserAPIClient user = new UserAPIClient();
+        try {
+            try {
+                user.register(emailField.getText(), nameField.getText(), passwordField.getText(), new APICallback() {
+                    @Override
+                    public void onSuccess(Object response) throws IOException {
+                        System.out.println("Success creating the user.");
+                    }
+
+                    @Override
+                    public void onError(Object error) {
+                        System.out.println("Error creating the user.");
+                    }
+                });
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+
 }
