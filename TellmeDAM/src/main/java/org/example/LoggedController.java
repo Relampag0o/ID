b5e4139b0eb0918c48db4490df555ef4988b1db2
Relampag0o <josemariar.l17@gmail.com>
2023-12-08 16:32:41 +0100

    .
    .

--- TellmeDAM/src/main/java/org/example/LoggedController.java
+++ TellmeDAM/src/main/java/org/example/LoggedController.java
@@ -4,11 +4,13 @@ import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXListView;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
@@ -34,7 +36,7 @@ public class LoggedController extends Application {
     public ScrollPane chatScrollPane;
     public VBox chatBox;
     public MFXTextField messageField;
-    public MFXListView listView;
+    public ListView listView;
     public MFXTextField searchField;
 
     public MFXButton chatButton;
@@ -61,6 +63,7 @@ public class LoggedController extends Application {
 
     public void initialize() {
         loadUsers();
+        //addCustomCell(App.userLogged);
         loadChat(App.userLogged);
 
     }
@@ -118,61 +121,27 @@ public class LoggedController extends Application {
             public void onSuccess(Object response) {
                 chats = new ArrayList<>((List<Chat>) response);
                 List<Message> messages = new ArrayList<>();
+                System.out.println("Chats loaded: " + chats.size());
 
-                // Extract the last message from each chat and add it to the messages list
                 for (Chat chat : chats) {
-                    // Use the MessageAPIClient to get the messages for each chat
+                    System.out.println("Chat id: " + chat.getId());
+                    System.out.println("User 1: " + chat.getUser1_username());
+                    System.out.println("User 2: " + chat.getUser2_username());
                     MessageAPIClient messageAPIClient = new MessageAPIClient();
-                    try {
-                        messageAPIClient.getMessagesFromChat(chat.getId(), new APICallback() {
-                            @Override
-                            public void onSuccess(Object response) {
-                                List<Message> chatMessages = (List<Message>) response;
-                                if (!chatMessages.isEmpty()) {
-                                    Message lastMessage = chatMessages.get(chatMessages.size() - 1);
-                                    messages.add(lastMessage);
-                                }
-                            }
 
-                            @Override
-                            public void onError(Object error) {
-                                // Handle error
-                            }
-                        });
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
                 }
                 listView.setCellFactory(param -> {
-                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("userchats.fxml"));
                     try {
-                        fxmlLoader.load();
+                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("userchats.fxml"));
+                        Parent root = fxmlLoader.load();
+                        UserChatsController controller = fxmlLoader.getController();
+                        controller.setChat(user);
+                        return controller;
                     } catch (IOException e) {
                         e.printStackTrace();
+                        return null;
                     }
-                    UserChatsController controller = fxmlLoader.getController();
-
-                    return new ListCell<Message>() {
-                        @Override
-                        protected void updateItem(Message message, boolean empty) {
-                            super.updateItem(message, empty);
-                            if (empty || message == null) {
-                                setGraphic(null);
-                                setText(null);
-                            } else {
-                                //User user = getUserFromMessage(message); // Necesitarás implementar este método
-                                controller.setChat(user, message);
-                                setGraphic(fxmlLoader.getRoot());
-                            }
-                        }
-                    };
                 });
-
-                listView.getItems().addAll(messages);
-                listView.setItems(FXCollections.observableArrayList(messages));
-
-
-
             }
 
             @Override
@@ -180,6 +149,28 @@ public class LoggedController extends Application {
                 // Handle error
             }
         });
+    }
+
+    private void addCustomCell(User user) {
+        try {
+            // Carga el archivo FXML para la celda personalizada
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("userchats.fxml"));
+
+            // Crea una nueva instancia de UserChatsController
+            Parent root = fxmlLoader.load();
+
+            // Configura la celda con los datos del usuario
+            UserChatsController controller = fxmlLoader.getController();
+            controller.setChat(user);
+
+            // Agrega la celda personalizada a la ListView
+            Platform.runLater(() -> {
+                listView.getItems().add(controller);
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void createChat(User user) {
--- TellmeDAM/src/main/java/org/example/UserChatsController.java
+++ TellmeDAM/src/main/java/org/example/UserChatsController.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class UserChatsController {
+public class UserChatsController extends ListCell<User> {
     @FXML
     private ImageView imageView;
     @FXML
@@ -13,11 +16,30 @@ public class UserChatsController {
     @FXML
     private Label messageLabel;
 
-    public void setChat(User user, Message message) {
+    @Override
+    protected void updateItem(User user, boolean empty) {
+        super.updateItem(user, empty);
+        if (empty || user == null) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("userchats.fxml"));
+                Parent root = loader.load();
+                UserChatsController controller = loader.getController();
+                controller.setChat(user);
+                setGraphic(root);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void setChat(User user) {
         Image img = new Image(user.getPhotourl());
         imageView.setImage(img);
         usernameLabel.setText(user.getUsername());
-        messageLabel.setText(message.getContent());
+        messageLabel.setText("");
     }
 }
 
