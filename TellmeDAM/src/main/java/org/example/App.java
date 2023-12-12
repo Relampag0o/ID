c6a3fe6017adf14842eb8e3aef0e950da1108e16
Relampag0o <josemariar.l17@gmail.com>
2023-12-12 02:41:53 +0100

    .
    .

--- TellmeDAM/src/main/java/org/example/App.java
+++ TellmeDAM/src/main/java/org/example/App.java
@@ -29,7 +29,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 650, 400);
+        scene = new Scene(loadFXML("primary"), 650, 450);
         stage.setScene(scene);
         stage.show();
     }
