0f1e161a78d73da568482d5a5be8679c9b54af2e
Relampag0o <josemariar.l17@gmail.com>
2023-12-14 01:11:54 +0100

    .
    .

--- PRACTISEListView/src/main/java/org/example/PrimaryController.java
+++ PRACTISEListView/src/main/java/org/example/PrimaryController.java
@@ -6,6 +6,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 
 public class PrimaryController {
 
@@ -40,15 +43,31 @@ public class PrimaryController {
 
         listView.setOnMouseClicked(event -> {
             Person selectedPerson = (Person) listView.getSelectionModel().getSelectedItem();
-            Tab tab = new Tab(selectedPerson.getName() + (tabPane.getTabs().size() + 1));
-            Button closeButton = new Button();
-            closeButton.setText("X");
-            tab.setClosable(true);
-            closeButton.setOnAction(event1 -> tabPane.getTabs().remove(tab));
-            tab.setGraphic(closeButton);
-            tabPane.getTabs().add(tab);
-            tab.setContent(new TextArea(selectedPerson.getPersonalInfo()));
 
+            // Crear una nueva pestaña
+            Tab tab = new Tab();
+
+            // Crear el contenido de la pestaña
+            HBox tabContent = new HBox();
+            Label nameLabel = new Label(selectedPerson.getName());
+            Button closeButton = new Button("X");
+
+            // Asignar estilos o propiedades según sea necesario
+            nameLabel.setStyle("-fx-padding: 0 5 0 0;");
+            closeButton.setStyle("-fx-background-color: transparent;");
+
+            // Asignar el evento para cerrar la pestaña
+            closeButton.setOnAction(e -> tabPane.getTabs().remove(tab));
+
+            // Agregar el contenido a la pestaña
+            Region spacer = new Region();
+            HBox.setHgrow(spacer, Priority.ALWAYS);
+            tabContent.getChildren().addAll(nameLabel, spacer, closeButton);
+            tab.setGraphic(tabContent);
+
+            // Agregar la pestaña al TabPane
+            tabPane.getTabs().add(tab);
         });
     }
 }
+
